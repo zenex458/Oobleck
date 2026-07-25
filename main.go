b7d5c0986c81cfbb5a93c724d4eb0d661fbf960b
ShaiyaJ <93013772+ShaiyaJ@users.noreply.github.com>
2026-07-25 13:27:26 +0000

    output format adjusted to add more space between tests

--- main.go
+++ main.go
@@ -74,7 +74,7 @@ func main() {
 
         // Running tests
         if check.(func() bool)() {
-            fmt.Printf("---- %s ----\nNAME: %s\nDESCRIPTION: %s\nCATEGORIES: %s\nFIX DESCRIPTIONS: %s\n", path, *name.(*string), *description.(*string), *categories.(*[]string), fixdescription.(func() string)())
+            fmt.Printf("---- %s ----\nNAME: %s\nDESCRIPTION: %s\nCATEGORIES: %s\nFIX DESCRIPTIONS: %s\n\n", path, *name.(*string), *description.(*string), *categories.(*[]string), fixdescription.(func() string)())
         }
 
         return nil
